f0fa7a296a137ea6ffa80adf083c84f68d6915fd
robert <rhsu123@gmail.com>
2017-01-23 15:15:20 -0800

    sone of a bitch...

--- ElasticDotNet/Program.cs
+++ ElasticDotNet/Program.cs
@@ -1,16 +1,36 @@
 ﻿using System;
-using Gtk;
+using Nest;
 
 namespace ElasticDotNet
 {
+	public class Tweet
+	{
+		public int Id { get; set; }
+		public string User { get; set; }
+		public DateTime PostDate { get; set; }
+		public string Message { get; set; }
+	}
+
 	class MainClass
 	{
 		public static void Main(string[] args)
 		{
-			Application.Init();
-			MainWindow win = new MainWindow();
-			win.Show();
-			Application.Run();
+			var node = new Uri("http://localhost:9200");
+			var settings = new ConnectionSettings(node);
+			settings.DefaultIndex("mytweetindex");
+
+			var client = new ElasticClient(settings);
+
+			var tweet = new Tweet
+			{
+				Id = 1,
+				User = "kimchy",
+				PostDate = new DateTime(2009, 11, 15),
+				Message = "Trying out NEST, so far so good?"
+			};
+
+			var response = client.Index(tweet);
+			Console.WriteLine(response.DebugInformation);
 		}
 	}
 }
