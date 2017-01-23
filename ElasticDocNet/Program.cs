5f1c7e4b1967a3fd17d020657300a05b1b7ac9d1
robert <rhsu123@gmail.com>
2017-01-23 10:39:40 -0800

    Hello Elastic World Created

---
+++ ElasticDocNet/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Gtk;
+using Nest;
+
+namespace ElasticDocNet
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			var client = new ElasticClient();
+		}
+	}
+}
